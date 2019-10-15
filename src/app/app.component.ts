8f2635cec331ad13dc70e7b1b79f916eb5bf729e
Tyler Tolson <tylertolson1@gmail.com>
2019-10-15 17:13:05 -0400

    Made the navbar sticky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'claymation';
   navLinks = ['Sculptures', 'Inktober' /* , 'Videos' */];
   lowerCase = (item: string) => item.toLowerCase();
+  myFunction(navbar: HTMLElement, sticky: number) {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add('sticky');
+      } else {
+        navbar.classList.remove('sticky');
+      }
+    }
+
+  ngOnInit() {
+    const navbar = document.getElementById('head-nav');
+    const sticky = navbar.offsetTop;
+    window.onscroll = () =>  this.myFunction(navbar, sticky);
+  }
 }
